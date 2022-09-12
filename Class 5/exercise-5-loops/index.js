dfb804e086d524181046323ea1e82a4ee170fa97
MasonT001 <112119657+MasonT001@users.noreply.github.com>
2022-09-12 12:59:53 -0500

    add class 5

--- Class 5/exercise-5-loops/index.js
+++ Class 5/exercise-5-loops/index.js
@@ -12,3 +12,14 @@ let people = [
       age: 23
     }
   ]
+
+for(let i = 0; i < people.length; i++){
+    console.log(people[i])
+    if (people.name == "John"){
+      console.log("I found John!")
+        } else if(people.name == "Jane") {
+      console.log("Hey Jane, do you know where John is?")
+    } else {
+      console.log("Hey do you know someone by the name of John?")
+    }
+}
