182da03aa0e5ca6ca5e5698decb7e38b355d2c18
MasonT001 <mltrepagnier@gmail.com>
2022-09-26 20:34:17 -0700

    Exercise practice

---
+++ Class 10/exercise 11/main.js
@@ -0,0 +1,5 @@
+let numbers = [1, 5, 10, 15, 20]
+
+numsGreaterThan10 = numbers.filter(value => value >= 10).length
+
+console.log(numsGreaterThan10)
---
+++ Class 10/exercise 12/main.js
@@ -0,0 +1,21 @@
+// If statement example
+ i = 0;
+// if ( i <= 50){
+//     console.log(i)
+// }
+
+// else if example
+
+// if ( i <= 50){
+//     console.log(i)
+// }else if(i > 34){
+//     console.log("we goin up")
+// }
+
+// else example
+
+if (i < 50){
+    console.log("Banans")
+}else if(i < 34){
+    console.log("we goin up")
+}else{console.log("No fun")}
