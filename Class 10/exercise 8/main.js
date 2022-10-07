a4cb4c5509abbbde791c062dfac23a523e20b211
MasonT001 <mltrepagnier@gmail.com>
2022-10-06 18:09:00 -0700

    Angular basics practice

--- Class 10/exercise 8/main.js
+++ Class 10/exercise 8/main.js
@@ -1,16 +1,16 @@
-a = 5;
-b = 5;
+// a = 5;
+// b = 5;
 
 
-console.log("a === b is ", a === b)
+// console.log("a === b is ", a === b)
 
-console.log("a !== b is ", a !== b)
+// console.log("a !== b is ", a !== b)
 
-console.log("a > b is ", a > b)
+// console.log("a > b is ", a > b)
 
-console.log("a < b is ", a < b)
+// console.log("a < b is ", a < b)
 
-console.log("a >= b is ", a >= b)
+// console.log("a >= b is ", a >= b)
 
-console.log("a <= b is ", a <= b)
+// console.log("a <= b is ", a <= b)
 
